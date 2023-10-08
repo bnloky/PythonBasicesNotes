a9d66efb6a676ad36a1216acbadfd94cc543f765
N Lokesh <lokeshnarasimha353@gmail.com>
2023-10-08 05:31:32 -0700

    2files

---
+++ Day24/staticmethodexample.py
@@ -0,0 +1,12 @@
+class lokymath:
+     
+     @staticmethod
+     def add(x,y):
+          print('the sum of:',x+y)
+
+     @staticmethod
+     def product(x,y):
+        print('the product of:',x*y)
+
+lokymath.add(10,20)
+lokymath.product(10,20)
